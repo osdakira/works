a51c16488fc328d50f14889f066a86900fde78ae
Akira Osada <osada@gu3.co.jp>
2012-02-26 21:48:57 +0900

    deploy時にskypeに飛ばす

--- deploy.rb
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-chat_id="#hoge"
-before :deploy, :roles => :db, :except => { :no_release => true } do
-  result = system("rubyskype/wisp.rb '%s' '[%s:%s:%s] Deploy start.'" % [chat_id, application, stage, branch])
-end
-after :deploy, :roles => :db, :except => { :no_release => true } do
-  result = system("rubyskype/wisp.rb '%s' '[%s:%s:%s] Deploy end.'" % [chat_id, application, stage, branch])
-end
